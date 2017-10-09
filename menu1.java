38fe3fc72ca78942a26239f0585cbd51974c59f3
Sawepeter <sawepeter6@gmail.com>
2017-10-10 02:34:37 +0300

    first  fragment java file(menu1.java)
    its one of my fragments

---
+++ menu1.java
@@ -0,0 +1,28 @@
+package com.example.sawepeter.navigation;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ *
+ * Created by SAWE PETER on 10/8/2017.
+ */
+
+public class menu1 extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.menu1, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        getActivity().setTitle("menu1");
+    }
+}
