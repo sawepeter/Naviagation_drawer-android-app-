ec134797c764da13295f23dfdef4955a1c9cfc76
Sawepeter <sawepeter6@gmail.com>
2017-10-10 02:35:43 +0300

    second fragment(menu2.java)

---
+++ menu2.java
@@ -0,0 +1,27 @@
+package com.example.sawepeter.navigation;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by SAWE PETER on 10/9/2017.
+ */
+
+public class menu2 extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.menu2, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        getActivity().setTitle("menu2");
+    }
+}
